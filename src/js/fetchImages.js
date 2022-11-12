b1ea023941f4d1094eb4ce8280b76f48b4b08900
Sviatoslav43 <navolskiy17@gmail.com>
2022-11-12 11:59:04 +0200

    homework

---
+++ src/js/fetchImages.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// Fetch images from Pixabay API using Axios
+
+async function fetchImages(name, page, perPage) {
+  const baseURL = 'https://pixabay.com/api/';
+  const key = '31260556-12ba6cd11582ca12a10f5b3d6';
+
+  try {
+    const response = await axios.get(
+      `${baseURL}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
+}
+
+export { fetchImages };
